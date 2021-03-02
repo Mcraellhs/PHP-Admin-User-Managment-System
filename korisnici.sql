8820143713059b938ba1f8f0f4002a0ddf84267f
Ahmed Sakic <sakicahmed@yahool.com>
2021-03-02 16:51:52 +0100

    update

--- korisnici.sql
+++ korisnici.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2021 at 12:47 AM
+-- Generation Time: Mar 02, 2021 at 04:50 PM
 -- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -72,12 +72,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `Ime`, `Prezime`, `Password`, `Username`, `Email`, `Administrator`) VALUES
 (2, 'Chuck', 'Norris', 'admin123', 'admin', 'Chucknorris@gmail.com', 1),
-(7, 'Safer', 'Zildzic', '', 'safke', 'safer@gmail.com', 2),
 (9, 'Connor', 'Mcgregor', '1234', 'notorious', 'conormcgregor@gmail.com', 2),
 (12, 'Ahmed', 'Sakic', 'ahmo', 'sakicahmed18', 'sakicahmed@gmail.com', 1),
-(13, 'Jon', 'Jones', '123', 'bones', 'bonesjones@gmail.com', 0),
 (16, 'Nikki', 'Lauda', '123', 'lauda1', 'sofrmail@mail.com', 0),
-(17, 'Hade', 'Hadeis', '123', 'Hades', 'maybe@gmail.com', 0);
+(17, 'Hade', 'Hadeis', '123', 'Hades', 'maybe@gmail.com', 0),
+(19, 'Guest', 'Guest', '123', 'guest', 'guest@guest.com', 2);
 
 -- --------------------------------------------------------
 
@@ -96,10 +95,9 @@ CREATE TABLE `users_predmet` (
 --
 
 INSERT INTO `users_predmet` (`id`, `user_id`, `predmet_id`) VALUES
-(7, 7, 1),
-(8, 7, 3),
 (14, 9, 4),
-(15, 9, 3);
+(15, 9, 3),
+(16, 19, 1);
 
 --
 -- Indexes for dumped tables
@@ -140,13 +138,13 @@ ALTER TABLE `predmet`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users_predmet`
 --
 ALTER TABLE `users_predmet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
